d32d3533dfbbaa0da04d977c8d64485e32b4c413
Aron Barcsa-Szabo <abarcsa@gmail.com>
2023-05-19 00:55:43 +0200

    Add backend folder

---
+++ backend/facebook-scraper/scrape_contacts.py
@@ -0,0 +1,5 @@
+from facebook_scraper import get_profile
+
+
+def get_profile_contacts(profile_id):
+    return get_profile(profile_id, credentials=('jackiesmithie2023@freemail.hu', 'CraftHackathlon01'))
